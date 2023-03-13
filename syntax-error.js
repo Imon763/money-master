f0bc5ab806809a3c11bf660e778d2c9539ab889d
ImonShah <Imonshah763@gmail.com>
2023-03-13 09:39:41 +0600

    first commit

---
+++ syntax-error.js
@@ -0,0 +1,111 @@
+/* >>=========================Syntax Error ==============================>> */
+
+// function isBigger(num) {
+//     if (num > 5) {
+//         return true;
+//     }
+//     else{
+//         return false
+//     }
+// }
+// // }
+
+// const retult = isBigger(11);
+// console.log(retult);
+
+// // const sum = a, b => a+b;
+// const sum = (a, b) => a+b;
+// const res = sum(3, 5);
+
+
+// const musk ={
+//     name: 'elon',
+//     // age: 53
+//     age: 53,
+//     job: 'tesla'
+// }
+
+// console.log(musk.age);
+
+// const numbers = [12, 13, 21, 3, 34, 4, 4,]
+
+// for(let i = 0; i < 10; i++){
+//     // const n = numbers[i;
+//     const n = numbers[i];
+// }
+
+// console.log(numbers)
+
+// // if(numbers.length > 5) && (numbers[0] === 5){
+
+// // }
+
+// if(numbers.length > 5 && numbers[0] === 5){
+
+// }
+
+
+
+
+
+
+
+/* >>=========================Type Error ==============================>> */
+
+// const numbs = 5;
+
+// numbs.push(45);
+
+// let address;
+// let address = {
+//     city: 'bBaria', postCode: 4560
+// } ;
+
+// // console.log(address.home.building);
+// console.log(address.home?.building);
+
+// const a = 'imon';
+// a= 'shah'
+
+
+
+
+
+
+/* >>=========================Reference Error ==============================>> */
+
+
+// const x = 'Pori'
+// const y = 'moni'
+// // console.log(z);
+
+// // console.log(name);
+// // const name = 'Imon Shah';
+// // let name = 'Imon Shah';
+// // var name = 'Imon Shah';
+
+// // console.log(add(2,3));
+
+// function add(n1, n2){
+//     return n1 + n2
+// }
+
+
+// const add2 = (n1, n2) => n1 + n2;
+
+
+// function doSomething () {
+//     const sum = 55;
+//     // return sum;
+// }
+
+// console.log(sum);
+
+// // NOTE :-> arrow function diye jodi kono kisu declare kori tahole setake function er pore initialize korte hobe. Arrow function er khetre age declare korle reference error hoite pare.
+// // Name likhte giye jodi name likhte vul kori tahole referrence error hobe
+
+
+
+
+
+/* >>=========================<+>==============================>> */
